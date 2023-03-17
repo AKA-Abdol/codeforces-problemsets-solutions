fcc72f687f47088d4f43c3b154be7612fba2bc67
Amirhossein <amirabdolii@gmail.com>
2023-03-17 23:11:32 +0330

    hotfix: output of boyOrGirl revised

--- A.BoyOrGirl/A.BoyOrGirl.py
+++ A.BoyOrGirl/A.BoyOrGirl.py
@@ -3,4 +3,8 @@ alphabet_flag = [0] * 26
 for char in username:
     alphabet_flag[ord(char) - ord('a')] = 1
 
-print(sum(alphabet_flag))
+is_male = sum(alphabet_flag) % 2
+if is_male:
+    print('IGNORE HIM!')
+else:
+    print('CHAT WITH HER!')
